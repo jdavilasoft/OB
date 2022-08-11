806cd5ce0767b68ade42e6d9c20fb4f9b011b942
Javier Dávila <jdavila.soft@gmail.com>
2022-08-10 23:00:02 -0500

    Create Coche.java

---
+++ pe/uebos/Coche.java
@@ -0,0 +1,21 @@
+package pe.uebos;
+
+public class Coche {
+
+	private int numPuertas = 0;
+
+	public static void main(String[] args) {
+		Coche miCoche = new Coche();
+		miCoche.incrementaNumPuertas(1);
+
+		System.out.println(miCoche);
+	}
+
+	public void incrementaNumPuertas(int numPuertas) {
+		this.numPuertas = numPuertas;
+	}
+
+	public String toString() {
+		return "El coche, tiene " + this.numPuertas + " puertas.";
+	}
+}
